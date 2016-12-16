76159cfd7c2cbb22f9f68728b3fb1937cc2d907e
Богомолов Илья Вадимович <ilyabog@outlook.com>
2016-12-16 19:05:06 +0300

    Изменить ACL права файла: удалить запись

---
+++ delete_record.sh
@@ -0,0 +1,55 @@
+#!/bin/bash 
+
+DEBUG=0
+
+PS3='Выберите, для кого удалить запись в ACL правах файла (q - выход): '
+# Перечень пунктов меню
+TARGET_OPTIONS=(
+    "для пользователя"
+    "для группы"
+)
+
+TARGETS=(
+    "u"
+    "g"
+)
+
+select opt in "${TARGET_OPTIONS[@]}"; do
+    #отладочная информация
+    if [[ $DEBUG == 1 ]]; then
+        echo "filename=$1"
+        echo "opt=$opt" 
+        echo "REPLY=$REPLY"
+    fi
+    #выход
+    if [[ $REPLY == "q" ]]; then
+        exit 1
+    fi
+    case $REPLY in 
+		[1-${#TARGET_OPTIONS[*]}])
+	    	PARAM=${TARGETS[$((REPLY-1))]}
+			if [[ $DEBUG == 1 ]]; then       
+				echo "Выбрано: $PARAM"
+			fi
+            break
+            ;;
+        q) break;;
+        *) echo "Неверный пункт меню" >&2;;
+    esac
+done
+
+PS3='Выберите пользователя или группу из списка (q - выход): '
+    select USER in $(getent passwd | cut -d: -f1); do
+        if [[ $REPLY == "q" ]]; then
+            exit 1
+        fi
+        if [[ -z $USER ]]; then
+            echo "Неверный выбор" >&2
+            continue
+        fi
+        echo "Вы выбрали '$USER'"
+        break
+    done
+
+setfacl -x "$PARAM":"$USER" "$1"
+getfacl "$1"
