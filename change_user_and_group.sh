dd2c656fc3306dfa8401fe78c71627c8d40cb50d
Иванов Антон Владимирович <ant4731@yandex.ru>
2016-12-21 23:12:20 +0300

    Добавил везде возможность выхода

--- change_user_and_group.sh
+++ change_user_and_group.sh
@@ -21,13 +21,16 @@ change_user()
 {
     PS3='Выберите пользователя из списка: '
     select user in $(getent passwd | cut -d: -f1); do
+	if [[ $REPLY == "q" ]]; then
+            exit 1
+    	fi 
         if [[ -z $user ]]; then
             echo "Неверный выбор" >&2
             continue
         fi
         echo "Вы выбрали пользователя '$user'."
-		chown $RECURSION $user $FILENAME
-		echo "Владельцом файла '$FILENAME'  является '$user'."
+	chown $RECURSION $user $FILENAME
+	echo "Владельцом файла '$FILENAME'  является '$user'."
         break
     done
 }
@@ -37,13 +40,16 @@ change_group()
 {
     PS3='Выберите группу из списка: '
     select group in $(cat /etc/group| cut -d: -f1); do
+	if [[ $REPLY == "q" ]]; then
+            exit 1
+    	fi 
 	if [[ -z $group ]]; then
     		echo "Неверный выбор" >&2
     		continue
         fi
         echo "Вы выбрали пользователя '$user'."
-		chown $RECURSION :$group $FILENAME
-		echo "Группой файла '$FILENAME'  является '$group'."
+	chown $RECURSION :$group $FILENAME
+	echo "Группой файла '$FILENAME'  является '$group'."
         break
     done
 }
