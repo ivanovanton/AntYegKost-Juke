cc422d7a270455b1b944634d37f6c4e84f402be3
yegor <vasilenko.yegor@gmail.com>
2016-12-22 23:15:28 +0300

    Исправил ошибки

--- add_record.sh
+++ add_record.sh
@@ -51,17 +51,52 @@ PS3='Выберите пользователя или группу из спис
         break
     done
 
-while :
-do
-    echo "Введите права доступа, которые требуется установить для данного пользователя или группы:"
-    read PERMISSION
-    if [[ $PERMISSION != "r" && $PERMISSION != "w" && $PERMISSION != "x" && $PERMISSION != "rw" && $PERMISSION != "rx" && $PERMISSION != "wx" && $PERMISSION != "rwx" ]]; then
-	echo "Введите корректные права"
-    else 
-	break
+HELP="Сценарий добавления записи в ACL правах файла"
+
+PS3='Выберите права доступа, которые необходимо установить для данного пользователя или группы (q - выход, help - справка): '
+ACTION_OPTIONS=(
+	"Добавить право чтения"
+	"Добавить право записи"
+	"Добавить право выполнения"
+	"Добавить право чтения и записи"
+	"Добавить право чтения и выполнения"
+	"Добавить право записи и выполнения"
+	"Добавить право чтения, записи и выполнения"
+)
+ACTIONS=(
+	"r"
+	"w"
+	"x"
+	"rw"
+	"rx"
+	"wx"
+	"rwx"
+)
+
+select opt in "${ACTION_OPTIONS[@]}"; do
+    if [[ $DEBUG == 1 ]]; then
+	echo "opt=$opt" 
+	echo "REPLY=$REPLY"
     fi
+    case $REPLY in 
+		[1-${#ACTION_OPTIONS[*]}])
+	    PERMISSION="${PERMISSION}${ACTIONS[$((REPLY-1))]}"
+	    	if [[ $DEBUG == 1 ]]; then       
+				echo "Выбрано: $PERMISSION"
+			fi
+	    break
+	    ;;
+	q) exit 1;;
+	"help") echo "$HELP"
+		 ;;
+	*) echo "Неверный пункт меню" >&2;;
+    esac
 done
-	
-#Добавляем для пользователя или группы те или иные права доступа
+
+if [[ -z $2 ]]; then
+setfacl -m "$PARAM":"$USER":"$PERMISSION" "$1"
+else
 setfacl "$2" -m "$PARAM":"$USER":"$PERMISSION" "$1"
+fi	
+#Добавляем для пользователя или группы те или иные права доступа
 getfacl "$1"
